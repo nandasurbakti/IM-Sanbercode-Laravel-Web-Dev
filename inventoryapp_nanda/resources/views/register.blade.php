88b7f57e49b17ab98adc3824eea75368097254c4
Nanda Surbakti <nandasurbakti13@gmail.com>
2025-10-28 17:28:26 +0700

    commit kesembilang: tugas laravel MVC

---
+++ inventoryapp_nanda/resources/views/register.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sign Up</title>
+  </head>
+  <body>
+    <h1>Buat Account Baru!</h1>
+    <h3>Sign Up Form</h3>
+    <form action="/welcome" method="POST">
+        @csrf
+      <label for="firstname">First name:</label><br /><br />
+      <input type="text" name="firstname" id="firstname" required /><br /><br />
+      <label for="lastname">Last name:</label><br /><br />
+      <input type="text" name="lastname" id="lastname" required /><br /><br />
+      <label for="gender">Gender:</label><br /><br />
+      <input type="radio" name="gender" id="male" value="male" />
+      <label for="male">Male</label><br />
+      <input type="radio" name="gender" id="female" value="female" />
+      <label for="female">Female</label><br />
+      <input type="radio" name="gender" id="others" value="others" />
+      <label for="others">Others</label><br /><br />
+      <label for="nationality">Nationality:</label><br /><br />
+      <select name="nationality" id="nationality">
+        <option value="indonesia">Indonesian</option>
+        <option value="malaysia">Malaysia</option>
+        <option value="singapore">Singapore</option></select
+      ><br /><br />
+      <label for="language">Language Spoken:</label><br /><br />
+      <input
+        type="checkbox"
+        name="language"
+        id="lang-indonesia"
+        value="bahasa-indonesia"
+      />
+      <label for="lang-indonesia">Bahasa Indonesia</label><br />
+      <input
+        type="checkbox"
+        name="language"
+        id="lang-english"
+        value="english"
+      />
+      <label for="lang-english">English</label><br />
+      <input type="checkbox" name="language" id="lang-other" value="other" />
+      <label for="lang-other">Other</label><br /><br />
+
+      <label for="bio">Bio:</label><br /><br />
+      <textarea name="bio" id="bio" rows="10" cols="30"></textarea><br />
+      <input type="submit" value="Sign Up" />
+    </form>
+  </body>
+</html>
